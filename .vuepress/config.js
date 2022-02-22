5185557928a3d389b955e97e3dbb26fb50b90129
ddshiyu <ddshiyu@163.com>
2022-02-22 19:13:17 +0800

    1

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -129,23 +129,6 @@ module.exports = {
     "startYear": "2021"
   },
   plugins: [
-    [
-      '@vuepress-reco/vuepress-plugin-kan-ban-niang',{
-        theme: [
-          'blackCat',  'miku', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'z16'
-        ],
-        clean: false,
-        messages: {
-          welcome: '欢迎来到我的博客', home: '心里的花，我想要带你回家。', theme: '好吧，希望你能喜欢我的其他小伙伴。', close: '你不喜欢我了吗？痴痴地望着你。' 
-        },
-        messageStyle: { left: '128px', bottom: '290px' },
-        modelStyle: { right: '-50px', bottom: '120px', opacity: '0.7' },
-        btnStyle: { left: '90px', bottom: '40px' },
-        clean: true,
-        width: 250,
-        height: 320
-      }
-    ],
     // ['@vuepress-reco/vuepress-plugin-bulletin-popover', {
     //   title: '公告',
     //   body: [
