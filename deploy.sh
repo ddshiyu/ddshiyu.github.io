2e451faaa5d7372b408cf0faae1106e9e1bf42db
ddshiyu <ddshiyu@163.com>
2023-10-27 22:32:32 +0800

    1

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+hugo
+cd ddshiyu.github.io
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:ddshiyu/ddshiyu.github.io.git master
+
+# cd ../
+# rm -rf public
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'push'
+git push -f git@github.com:ddshiyu/ddshiyu.github.io.git hugo
